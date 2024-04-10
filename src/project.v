0867a5cc84f93367a9b63c1a06ebe6bd09bb191c
Emily Schmidt <aiju@phicode.de>
2024-04-10 20:37:50 +0100

    make a silly counter

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_example (
+module tt_aiju (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,9 +16,15 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+	reg [7:0] ctr;
+
+	always @(posedge clk or negedge rst_n) begin
+		if(!rst_n)
+			ctr <= 0;
+		else
+			ctr <= ctr + 1;
+	end
+
+	assign uo_out = ctr;
 
 endmodule
