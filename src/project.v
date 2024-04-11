177b60863874ae2c4adfc8f545ba5ba3eb0dbb0e
Emily Schmidt <aiju@phicode.de>
2024-04-11 09:50:45 +0100

    make slightly less stupid cpu

--- src/project.v
+++ src/project.v
@@ -9,43 +9,158 @@ module tt_um_aiju (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    output reg [7:0] uio_out,  // IOs: Output path
+    output reg [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
 
-	reg [7:0] rPC, rIP;
-	reg [3:0] state;
+	reg memory_read, memory_write;
+	reg memory_done;
+	reg [15:0] memory_addr;
+	wire [7:0] memory_rdata;
+	reg [7:0] memory_wdata;
 
-	assign uio_oe = 8'b0;
-	assign uio_out = {7'b0, state == 0};
-	assign uo_out = rPC;
+	// TODO: synchroniser
+	wire handshake_in;
+	reg handshake_out;
+	assign handshake_in = ui_in[0];
+	assign uo_out = {memory_read, memory_write, handshake_out};
+	reg handshake_valid, handshake_ready;
+	reg handshake_state;
+	always @(posedge clk or negedge rst_n) begin
+		if(!rst_n) begin
+			handshake_ready <= 1'b0;
+			handshake_state <= 1'b0;
+			handshake_out <= 1'b0;
+		end else begin
+			handshake_ready <= 1'b0;
+			if(!handshake_state) begin
+				if(!handshake_in) begin
+					handshake_state <= 1'b1;
+				end
+			end else begin
+				if(handshake_valid) begin
+					handshake_out <= 1'b1;
+				end
+				if(handshake_in && handshake_out) begin
+					handshake_ready <= 1'b1;
+					handshake_out <= 1'b0;
+					handshake_state <= 1'b0;
+				end
+			end
+		end
+	end
+
+	reg [3:0] memory_state, memory_state_nxt;
+	assign memory_rdata = uio_in;
+	localparam MEMORY_IDLE = 0;
+	localparam MEMORY_ADDR_LOW = 1;
+	localparam MEMORY_ADDR_HIGH = 2;
+	localparam MEMORY_DATA = 3;
+
+	always @(posedge clk or negedge rst_n) begin
+		if(!rst_n) begin
+			memory_state <= MEMORY_IDLE;
+		end else begin
+			memory_state <= memory_state_nxt;
+		end
+	end
+
+	always @(*) begin
+		memory_state_nxt = memory_state;
+		uio_oe = 8'b0;
+		uio_out = 8'bx;
+		handshake_valid = 1'b0;
+		memory_done = 1'b0;
+		case(memory_state)
+		MEMORY_IDLE: begin
+			if(memory_read || memory_write) begin
+				memory_state_nxt = MEMORY_ADDR_LOW;
+			end
+		end
+		MEMORY_ADDR_LOW: begin
+			handshake_valid = 1'b1;
+			uio_oe = 8'hff;
+			uio_out = memory_addr[7:0];
+			if(handshake_ready) begin
+				memory_state_nxt = MEMORY_ADDR_HIGH;
+			end
+		end
+		MEMORY_ADDR_HIGH: begin
+			handshake_valid = 1'b1;
+			uio_oe = 8'hff;
+			uio_out = memory_addr[15:8];
+			if(handshake_ready) begin
+				memory_state_nxt = MEMORY_DATA;
+			end
+		end
+		MEMORY_DATA: begin
+			handshake_valid = 1'b1;
+			if(memory_write) begin
+				uio_oe = 8'hff;
+				uio_out = memory_wdata;
+			end
+			if(handshake_ready) begin
+				memory_done = 1'b1;
+				memory_state_nxt = MEMORY_IDLE;
+			end
+		end
+		endcase
+	end
+
+	reg [15:0] rPC;
+	reg [7:0] rA;
+	reg [7:0] rIR;
+	reg [3:0] state, state_nxt;
+	localparam CPU_FETCH = 0;
+	localparam EXECUTE = 1;
 
 	always @(posedge clk or negedge rst_n) begin
 		if(!rst_n) begin
 			rPC <= 0;
-			rIP <= 0;
-			state <= 0;
+			rIR <= 0;
+			state <= CPU_FETCH;
+			rA <= 0;
 		end else begin
+			state <= state_nxt;
 			case(state)
-			0: begin
-				state <= 1;
-			end
-			1: begin
-				rIP <= ui_in;
-				rPC <= rPC + 1;
-				state <= 2;
-			end
-			2: begin
-				if(rIP == 42)
-					state <= 2;
-				else
-					state <= 0;
+			CPU_FETCH:
+				if(memory_done) begin
+					rIR <= memory_rdata;
+					rPC <= rPC + 1;
+					state <= EXECUTE;
+				end
+			EXECUTE: begin
+				if(rIR == 0)
+					rA <= 0;
+				if(rIR == 1)
+					rA <= rA + 1;
+				if(rIR != 2 || memory_done)
+					state <= CPU_FETCH;
 			end
 			endcase
 		end
+	end
+
+	always @(*) begin
+		memory_addr = 16'bx;
+		memory_wdata = 8'bx;
+		memory_read = 1'b0;
+		memory_write = 1'b0;
+		state_nxt = state;
+		case(state)
+		CPU_FETCH: begin
+			memory_addr = rPC;
+			memory_read = 1'b1;
+		end
+		EXECUTE: begin
+			memory_addr = 16'hCAFE;
+			memory_wdata = rA;
+			memory_write = rIR == 2;
+		end
+		endcase
 	end
 
 endmodule
--- test/test.py
+++ test/test.py
@@ -3,15 +3,59 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, Timer
+
+def memory_read(addr):
+  if addr == 7:
+    return 2
+  return (addr % 7) > 0
+
+def memory_write(addr, data):
+  print(hex(addr), hex(data))
+
+async def bus_cycle(dut):
+  addr = 0
+  while (dut.uo_out.value & 1) == 0:
+    await ClockCycles(dut.clk, 1)
+  assert dut.uio_oe.value == 255
+  is_write = (dut.uo_out.value & 2) != 0
+  addr = dut.uio_out.value
+  dut.ui_in.value = 1
+  while (dut.uo_out.value & 1) != 0:
+    await ClockCycles(dut.clk, 1)
+  dut.ui_in.value = 0
+  while (dut.uo_out.value & 1) == 0:
+    await ClockCycles(dut.clk, 1)
+  assert dut.uio_oe.value == 255
+  addr = addr | dut.uio_out.value << 8
+  dut.ui_in.value = 1
+  while (dut.uo_out.value & 1) != 0:
+    await ClockCycles(dut.clk, 1)
+  dut.ui_in.value = 0
+  while (dut.uo_out.value & 1) == 0:
+    await ClockCycles(dut.clk, 1)
+  if is_write:
+    memory_write(addr, dut.uio_out.value)
+  else:
+    dut.uio_in.value = memory_read(addr)
+    await ClockCycles(dut.clk, 1)
+  dut.ui_in.value = 1
+  while (dut.uo_out.value & 1) != 0:
+    await ClockCycles(dut.clk, 1)
+  dut.ui_in.value = 0
+
+async def timeout(dut):
+  await Timer(1000, units='us')
+  assert False and "TIMED OUT"
 
 @cocotb.test()
 async def test_project(dut):
   dut._log.info("Start")
   
   # Our example module doesn't use clock and reset, but we show how to use them here anyway.
-  clock = Clock(dut.clk, 10, units="us")
+  clock = Clock(dut.clk, 1, units="us")
   cocotb.start_soon(clock.start())
+  cocotb.start_soon(timeout(dut))
 
   # Reset
   dut._log.info("Reset")
@@ -24,9 +68,8 @@ async def test_project(dut):
 
   # Set the input values, wait one clock cycle, and check the output
   dut._log.info("Test")
-  dut.ui_in.value = 20
-  dut.uio_in.value = 30
 
-  await ClockCycles(dut.clk, 100)
+  while True:
+    await bus_cycle(dut)
 
   # assert dut.uo_out.value == 50
